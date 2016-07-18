c1c6547e29370e4c3ecbdb1975922ab8a8ffde3d
Alex Sanford <alex.sanford1@gmail.com>
2016-07-18 11:34:11 -0300

    Add messages JSON resource

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+    respond_to do |format|
+      format.json { render json: @messages }
+    end
+  end
+
+  def create
+    @message = Message.new(params[:message])
+    respond_to do |format|
+      if @message.save
+        format.json { render json: @message }
+      else
+        format.json { render json: { errors: @message.errors.messages }, status: :unprocessible_entity }
+      end
+    end
+  end
 end
